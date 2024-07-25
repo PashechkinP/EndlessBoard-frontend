8e9c7f0a9e463a8657a85ac0ad20abd4bee40ac6
PashechkinP <xeunique@mail.ru>
2024-07-25 21:13:09 +0300

    added registration

--- src/components/login.js
+++ src/components/login.js
@@ -1,18 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react'
 import "./regform.css"
 import {TextField, Typography, Button, Stack} from "@mui/material";
 import {Link} from "react-router-dom";
+import { useEffect } from 'react';
+import { SmartButtonOutlined } from '@mui/icons-material';
 
 
-const Login = () => {
+function Login() {
 
-  return (
+  
+
+  const [showUsers, setShowUsers] = useState([]);
+  const [offset, setOffset] = useState(0);   
+  const [finduser, setFinduser] = useState('');
+
+  // переменная для смещения подгрузки новых постов
+  document.addEventListener("scroll", scrollListener);
+
+  useEffect(() => {  // то что должно сделаться при первом запуске так как указано []
+
+    let url = new URL("/api/v1/products/login", "http://localhost:8000");  // конструктор URL адресов
+    console.log(`Request! Offset ${offset}`)
+    url.searchParams.set('offset', offset);
+    let xhrFirstReq = new XMLHttpRequest();
+    xhrFirstReq.open('GET', url);
+    xhrFirstReq.setRequestHeader("Content-Type", "application/json")
+    xhrFirstReq.responseType = 'json';
+    xhrFirstReq.send();
+    xhrFirstReq.onreadystatechange = function () {     // если не пашет выводить в консоль console.log
+      if (xhrFirstReq.readyState != 4) return;
+      let responseFirst = xhrFirstReq.response;
+      console.log(responseFirst);
+      setShowUsers([...showUsers, ...responseFirst]);
+    }
+  }, [offset]);
+
+  function scrollListener(e) {
+    console.log(`Sroll detected! ${window.scrollY + 2} ${document.body.offsetHeight - window.innerHeight}`)
+    // if (window.scrollY + 2 > document.body.offsetHeight - window.innerHeight) {
+    //   setOffset(offset + 25)
+    // }
+  }
+
+  showUsers.map((userki) => <li>{userki.userokname} {userki.userokpass}</li>); // id обязательно надо для reacta
+
+  const [filteredUsers, setFilteredUsers] = useState([]);
+  const [isAuth, SetIsAuth] = useState(false);
+  const [Name, SetName] = useState("");
+  const [Password, SetPassword] = useState ("")
+  const handleFilter = (event)=>{
+    const value = event.target.value;
+    const filtered = showUsers.filter(user=>SetName(user.userokname.includes(`${finduser}`)));
+    setFilteredUsers(filtered);
+  }
+  
+  return (  
    
-    <div className='container'>
-            <Typography variant="h2" fontFamily='Poppins' textAlign='center'>Войти на сайт</Typography>
+    <div className='container'>      
+            <Typography variant="h2" fontFamily='Poppins' textAlign='center'></Typography>
             <Typography variant="body1" marginBottom={3} fontFamily='Poppins' textAlign='center'>Введите логин и пароль</Typography>
             <TextField
                 fullWidth={true}
+                value={finduser}
+                margin='normal'
+                label="Username"
+                variant="outlined"
+                placeholder="Введите ваш username"
+                onChange={(e) => {
+                setFinduser(e.target.value);}}
+            />
+                <Button onClick={handleFilter}> ПОКАЗАТЬ </Button>
+                <Typography>{filteredUsers.map(user => (<div>{user.userokname}</div>))}</Typography>
+                <TextField
+                fullWidth={true}
+                value={Name}
                 margin='normal'
                 label="Username"
                 variant="outlined"
--- src/components/registration.js
+++ src/components/registration.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import "./regform.css"
 import {TextField, Typography, Button, Stack} from "@mui/material";
+import { useState } from "react";
+import {Link} from "react-router-dom";
 
 const Registration = () => {
 
+  const [name1, setName1] = useState("");
+  const [name2, setName2] = useState("");
 
   return (
     <div className='container'>
@@ -11,29 +15,48 @@ const Registration = () => {
             <Typography variant="body1" marginBottom={3} fontFamily='Poppins' textAlign='center'>Введите данные для регистрации</Typography>
             <TextField
                 fullWidth={true}
+                value={name1}
                 margin='normal'
                 label="Username"
                 variant="outlined"
                 placeholder="Введите ваш username"
+                onChange={(e) => {
+                  setName1(e.target.value);
+              }}
             />
             <TextField
                 type="password"
+                value={name2}
                 fullWidth={true}
                 margin='normal'
                 label="Password"
                 variant="outlined"
                 placeholder="Введите ваш пароль"
+                onChange={(e) => {
+                  setName2(e.target.value);
+              }}
             />
-            <TextField
+            {/* <TextField
                 type="password"
                 fullWidth={true}
                 margin='normal'
                 label="Password"
                 variant="outlined"
                 placeholder="Повторите ваш пароль"
-            />
+            /> */}
            <Stack spacing={2} direction="row" justifyContent={"flex-end"} >
-             <Button variant="contained">Регистрация</Button>
+           <Link to='/'>
+             <Button variant="contained" onClick={
+              ()=>fetch('http://localhost:8000/api/v1/products/userok', {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({userokname: name1, userokpass: name2})
+              })
+              .then(res => res.json())
+              .then(data => console.log('Успешно:', data))
+              .catch(error => console.error('Ошибка:', error))
+            }>Регистрация</Button>
+            </Link>
           </Stack>
     </div>
   );
